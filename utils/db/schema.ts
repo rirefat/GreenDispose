427bf7b221d52e047c48a44b8a9c1bca0d493b11
rirefat <rirefat.official@gmail.com>
2024-09-27 01:49:49 +0600

    done reports db table

--- utils/db/schema.ts
+++ utils/db/schema.ts
@@ -1,7 +1,23 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+import { integer, jsonb, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
-export const users = pgTable('users', {
+// Users Table
+export const Users = pgTable('users', {
     id: serial('id').primaryKey(),
     name: text('name').notNull(),
     email: varchar('email', { length: 50 }).notNull().unique(),
-})
+    createdAt: timestamp('created_at').defaultNow().notNull()
+});
+
+// Reports Table
+export const Reports = pgTable('reports', {
+    id: serial('id').primaryKey(),
+    userId: integer('user_id').references(() => Users.id).notNull(),
+    location: text('location').notNull(),
+    wasteType: varchar('waste_type', { length: 255 }).notNull(),
+    amount: varchar('amount', { length: 255 }).notNull(),
+    imageUrl: text('image_url'),
+    verificationResult: jsonb('verification_result'),
+    status: varchar('status', { length: 255 }).notNull().default('pending'),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    collectorId: integer('collector_id').references(() => Users.id)
+});
