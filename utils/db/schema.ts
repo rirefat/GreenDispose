e7c14f8beecfc3c11f164921739e56a411050b10
rirefat <rirefat.official@gmail.com>
2024-09-27 01:56:12 +0600

    done rewards db table

--- utils/db/schema.ts
+++ utils/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, jsonb, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { boolean, integer, jsonb, pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
 // Users Table
 export const Users = pgTable('users', {
@@ -21,3 +21,17 @@ export const Reports = pgTable('reports', {
     createdAt: timestamp('created_at').defaultNow().notNull(),
     collectorId: integer('collector_id').references(() => Users.id)
 });
+
+// Rewards table
+export const Rewards = pgTable('rewards',{
+    id: serial('id').primaryKey(),
+    userId: integer('user_id').references(()=>Users.id).notNull(),
+    points: integer('points').notNull().default(0),
+    level: integer('level').notNull().default(1),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    isAvailable: boolean('is_available').notNull().default(true),
+    description: text('description'),
+    name: varchar('name', {length: 255}).notNull(),
+    collectionInfo: text('collection_info').notNull()
+})
