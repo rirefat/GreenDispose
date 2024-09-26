d0c03fbadeae0b8e3b27b4cc89af40149ab99760
rirefat <rirefat.official@gmail.com>
2024-09-27 02:02:08 +0600

    done collectedWastes db table

--- utils/db/schema.ts
+++ utils/db/schema.ts
@@ -23,15 +23,24 @@ export const Reports = pgTable('reports', {
 });
 
 // Rewards table
-export const Rewards = pgTable('rewards',{
+export const Rewards = pgTable('rewards', {
     id: serial('id').primaryKey(),
-    userId: integer('user_id').references(()=>Users.id).notNull(),
+    userId: integer('user_id').references(() => Users.id).notNull(),
     points: integer('points').notNull().default(0),
     level: integer('level').notNull().default(1),
     createdAt: timestamp('created_at').defaultNow().notNull(),
     updatedAt: timestamp('updated_at').defaultNow().notNull(),
     isAvailable: boolean('is_available').notNull().default(true),
     description: text('description'),
-    name: varchar('name', {length: 255}).notNull(),
+    name: varchar('name', { length: 255 }).notNull(),
     collectionInfo: text('collection_info').notNull()
+});
+
+// Collected Waste Table
+export const CollectedWaste = pgTable('collected_waste', {
+    id: serial('id').primaryKey(),
+    reportId: integer('report_id').references(() => Reports.id).notNull(),
+    collectorId: integer('collector_id').references(() => Users.id).notNull(),
+    collectionDate: timestamp('collection_date').notNull(),
+    status: varchar('status', { length: 20 }).notNull().default('collected')
 })
